5a4c0edc61e106ee6535f1e25186e2cf62d2ff7f
rubyfmzk <rubyfmzk@gmail.com>
2021-04-15 17:48:01 +0900

    resize images

--- image-manager/handler.py
+++ image-manager/handler.py
@@ -1,24 +1,58 @@
 import json
+import boto3
+import uuid
+from urllib.parse import unquote_plus
+import PIL
+from PIL import Image
+from io import BytesIO
+
+s3 = boto3.resource('s3')
+
+def resize_put_sabian_images(event, context):
+
+  #必要な範囲を入力
+  for symbol in range(2, 3):
+    for degree in range(16, 31):
+      key = str(symbol).zfill(2)+'/'+str(degree).zfill(2)+'.jpg'
+      _resize_put_sabian_images(key, 'sabian-symbols/full/')
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def resize_put_sabian_images_by_event(event, context):
+  for record in event['Records']:
+    key = unquote_plus(record['s3']['object']['key'])
+    _resize_put_sabian_images(key, '')
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
 
-    return response
+def _resize_put_sabian_images(key, org_key_prefix):
+  bucket = 'image-manager-rubyfmzk'
+  resize_bucket = 'sabian-symbols'
+  org_key = unquote_plus(org_key_prefix + key)
+  key = unquote_plus(key)
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+  _resize(bucket, resize_bucket, org_key, key, 480, 480)
+  _resize(bucket, resize_bucket, org_key, '90px/'+key, 90, 90)
+  _resize(bucket, resize_bucket, org_key, '1080px/'+key, 1080, 1080)
+
+
+def _resize(org_bucket, resize_bucket, org_key, resize_key, x, y):
+
+    obj = s3.Object(
+        bucket_name=org_bucket,
+        key=org_key,
+    )
+    obj_body = obj.get()['Body'].read()
+
+    img = Image.open(BytesIO(obj_body))
+    img = img.resize((x, y), PIL.Image.ANTIALIAS)
+    buffer = BytesIO()
+    img.save(buffer, 'JPEG')
+    buffer.seek(0)
+
+    obj = s3.Object(
+        bucket_name=resize_bucket,
+        key=resize_key,
+    )
+    obj.put(Body=buffer, ContentType='image/jpeg')
+
+
+
