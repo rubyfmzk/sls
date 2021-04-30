4a6c48c38c4b58b2df0f535d543351efa749e169
rubyfmzk <rubyfmzk@gmail.com>
2021-05-01 01:34:55 +0900

    add collage function

--- image-manager/handler.py
+++ image-manager/handler.py
@@ -5,11 +5,14 @@ from urllib.parse import unquote_plus
 import PIL
 from PIL import Image
 from io import BytesIO
+from boto3.dynamodb.conditions import Key
 import re
 
 
 s3 = boto3.resource('s3')
 bucket = 'image-manager-rubyfmzk'
+ddb = boto3.resource("dynamodb")
+table = ddb.Table('Collage')
 
 def resize_put_sabian_images(event, context):
 
@@ -25,16 +28,23 @@ def resize_put_sabian_images_by_event(event, context):
     key = unquote_plus(record['s3']['object']['key'])
     _resize_put_sabian_images(key, '')
 
-def collage_object_white_transparent(event, context):
+def collage(event, context):
   org_key = event['Records'][0]['s3']['object']['key']
-  img_name = org_key.replace('public/collage/object/', '')
+
+  collage_type = 'object'
+  if '/background/' in org_key:
+    collage_type = 'background'
+  elif '/color/' in org_key:
+    collage_type = 'color'
+
+  img_name = org_key.replace('public/collage/'+collage_type+'/', '')
   img_name = re.sub(r'\.\w+$', '', img_name)
-  full_key = 'collage/object/full/'+img_name+'.png'
-  thumbnail_key = 'collage/object/thumbnail/'+img_name+'.png'
+  full_key = 'collage/'+collage_type+'/full/'+img_name+'.png'
+  thumbnail_key = 'collage/'+collage_type+'/thumbnail/'+img_name+'.png'
 
   obj = s3.Object(
-      bucket_name=bucket,
-      key=org_key,
+    bucket_name=bucket,
+    key=org_key,
   )
   obj_body = obj.get()['Body'].read()
   img = Image.open(BytesIO(obj_body))
@@ -42,33 +52,74 @@ def collage_object_white_transparent(event, context):
   size = img_rgba.size
   img_new = Image.new('RGBA', size)
 
-  for x in range(size[0]):
-    for y in range(size[1]):
-      r,g,b,a = img_rgba.getpixel((x,y))
+  if collage_type == 'object':
+    for x in range(size[0]):
+      for y in range(size[1]):
+        r,g,b,a = img_rgba.getpixel((x,y))
 
-      alpha = int(255 - (r + g + b) / 3 * (255 / 200))
+        alpha = int(255 - (r + g + b) / 3 * (255 / 200))
 
-      if alpha > 255:
-        alpha = 255
-      elif alpha < 0:
-        alpha = 0
-      img_new.putpixel((x,y),(255, 255, 255, alpha))
+        if alpha > 255:
+          alpha = 255
+        elif alpha < 0:
+          alpha = 0
+        img_new.putpixel((x,y),(255, 255, 255, alpha))
 
-  buffer = BytesIO()
-  img_new.save(buffer, 'PNG')
-  buffer.seek(0)
+    buffer = BytesIO()
+    img_new.save(buffer, 'PNG')
+    buffer.seek(0)
 
-  obj = s3.Object(
-    bucket_name=bucket,
-    key=full_key,
-  )
-  obj.put(Body=buffer, ContentType='image/png')
+    obj = s3.Object(
+      bucket_name=bucket,
+      key=full_key,
+    )
+    obj.put(Body=buffer, ContentType='image/png')
+
+  elif collage_type == 'background':
+    for x in range(size[0]):
+      for y in range(size[1]):
+        r,g,b,a = img_rgba.getpixel((x,y))
+
+        bright = int((r + g + b) / 3)
+
+        if bright > 255:
+          bright = 255
+        elif bright < 0:
+          bright = 0
+        img_new.putpixel((x,y),(bright, bright, bright, 255))
+
+    buffer = BytesIO()
+    img_new.save(buffer, 'PNG')
+    buffer.seek(0)
+
+    obj = s3.Object(
+      bucket_name=bucket,
+      key=full_key,
+    )
+    obj.put(Body=buffer, ContentType='image/png')
+
+  elif collage_type == 'color':
+    source= { 'Bucket' : bucket, 'Key': org_key}
+    dest = s3.Bucket(bucket)
+    dest.copy(source, full_key)
 
   #thumbnail
   long_side = size[0] if size[0] > size[1] else size[1]
   thumbnail_x = int(size[0] / long_side * 100)
   thumbnail_y = int(size[1] / long_side * 100)
   _resize(bucket, bucket, full_key, thumbnail_key, thumbnail_x, thumbnail_y, 'PNG')
+
+
+def api_collage_list(event, context):
+  data = table.scan()
+  return data
+
+
+def api_collage_list_by_motif(event, context):
+  data = table.query(
+    KeyConditionExpression = Key("motif").eq(event['path']['motif'])
+  )
+  return data
 
 
 def _resize_put_sabian_images(key, org_key_prefix):
